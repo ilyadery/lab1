f1ade74544b64b05bf9706bb0531ec044683f5f6
ilyadery <55979662+ilyadery@users.noreply.github.com>
2019-09-30 15:18:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,21 +8,32 @@ $first_number = $_POST['last'];
 $second_number = $_POST['next'];
 $third_number = $_POST['tarif'];
 
+if(enter($first_number) && enter($second_number) && enter($third_number) && $first_number > $second_number) {
+    $res = ($first_number - $second_number) * $third_number;
+} else {
+    echo "Ошибка,введите корректные данные!";
+}
 
-$answer = ($first_number - $second_number) * $third_number;
-echo "Сколько мы должны: " . $answer . "<BR>";
+function enter($num)
+{
+   return is_numeric($num) && $num > 0;
+}
+//echo "Сколько мы должны: " . $answer . "<BR>";
+
 ?>
 <BR>
 <form method="POST" action="index.php">
     Прошлый месяц: <BR>
-    <input name="last" type="number"  placeholder="введите прошлый показания"><BR>
+    <input name="last" type="number" placeholder="Введите прошлый показатель"><BR>
     Этот месяц: <BR>
-    <input name="next" type="text" ><BR>
+    <input name="next" type="number"><BR>
     Тариф: <br>
-    <input name="tarif" type="text" required>
+    <input name="tarif" type="number" required>
     <BR>
     <BR>
     <input type="submit" value="Вычислить">
+    <div label>Результат: </label><input name="result" id="calc_result" value="<?php echo $res ?>"  disabled="disabled" size="4" />
+    </div>
 </form>
 </body>
 </html>
